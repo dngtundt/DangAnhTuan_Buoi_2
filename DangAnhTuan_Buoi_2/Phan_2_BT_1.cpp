4651d6766ba0556385cf6d797e2de3fc8d638705
dngtundt <dtun11111@gmail.com>
2024-07-17 14:48:06 +0700

    Phan 2 BT 1 added menu show and main

--- DangAnhTuan_Buoi_2/Phan_2_BT_1.cpp
+++ DangAnhTuan_Buoi_2/Phan_2_BT_1.cpp
@@ -1,6 +1,9 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define MAX_SIZE 100
 
 // Hàm tạo mảng ngẫu nhiên
 void generate_random_array(int* a, int n, int min, int max) {
@@ -93,4 +96,158 @@ void quick_sort(int* a, int left, int right) {
     }
     quick_sort(a, left, j);
     quick_sort(a, i, right);
+}
+// Hàm in mảng
+void print_array(int* a, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+}
+
+// Hàm hiển thị menu
+void display_menu() {
+    printf("=== MENU ===\n");
+    printf("1. Generate random array\n");
+    printf("2. Generate even array\n");
+    printf("3. Tim kiem tuyen tinh\n");
+    printf("4. Sap xep tang dan Interchange Sort\n");
+    printf("5. Sap xep giam dan Interchange Sort\n");
+    printf("6. Tim kiem nhi phan\n");
+    printf("7. Sap xep tang dan Selection Sort\n");
+    printf("8. Sap xep tang dan Quick Sort\n");
+    printf("9. Exit\n");
+    printf("==============\n");
+}
+
+int main() {
+    srand(time(NULL));
+
+    int n = 0;
+    int a[MAX_SIZE];
+    int b[MAX_SIZE];
+    int m = 0;
+    int choice;
+    int x;
+
+    while (1) {
+        display_menu();
+        printf("Chon chuc nang: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("Nhap so luong cua mang (>= 15): ");
+            scanf("%d", &n);
+            if (n < 15) {
+                printf("So luong phan tu phai lon hon hoac bang 15!\n");
+            }
+            else {
+                generate_random_array(a, n, 0, 100);
+                printf("mang ngau nhien: ");
+                print_array(a, n);
+            }
+            break;
+
+        case 2:
+            if (n == 0) {
+                printf("Hay tao mang ngau nhien truoc!\n");
+            }
+            else {
+                generate_even_array(a, b, n, &m);
+                printf("Mang chua toan so chan: ");
+                print_array(b, m);
+            }
+            break;
+
+        case 3:
+            if (n == 0) {
+                printf("Hay tao mang ngau nhien truoc!\n");
+            }
+            else {
+                printf("Nhap gia tri can tim: ");
+                scanf("%d", &x);
+                int index = linear_search(a, n, x);
+                if (index != -1) {
+                    printf("Tim thay %d o vi tri %d bang Linear Search.\n", x, index);
+                }
+                else {
+                    printf("Khong tim thay %d bang Linear Search.\n", x);
+                }
+            }
+            break;
+
+        case 4:
+            if (n == 0) {
+                printf("Hay tao mang ngau nhien truoc!\n");
+            }
+            else {
+                interchange_sort(a, n, 1);
+                printf("Mang sau khi sap xep tang dan bang Interchange Sort: ");
+                print_array(a, n);
+            }
+            break;
+
+        case 5:
+            if (n == 0) {
+                printf("Hay tao mang ngau nhien truoc!\n");
+            }
+            else {
+                interchange_sort(a, n, 0);
+                printf("Mang sau khi sap xep giam dan bang Interchange Sort: ");
+                print_array(a, n);
+            }
+            break;
+
+        case 6:
+            if (n == 0) {
+                printf("Hay tao mang ngau nhien truoc!\n");
+            }
+            else {
+                interchange_sort(a, n, 1);
+                printf("Nhap gia tri can tim: ");
+                scanf("%d", &x);
+                int index = binary_search(a, n, x);
+                if (index != -1) {
+                    printf("Tim thay %d o vi tri %d bang Binary Search.\n", x, index);
+                }
+                else {
+                    printf("Khong tim thay %d bang Binary Search.\n", x);
+                }
+            }
+            break;
+
+        case 7:
+            if (n == 0) {
+                printf("Hay tao mang ngau nhien truoc!\n");
+            }
+            else {
+                selection_sort(a, n);
+                printf("Mang sau khi sap xep tang dan bang Selection Sort: ");
+                print_array(a, n);
+            }
+            break;
+
+        case 8:
+            if (n == 0) {
+                printf("Hay tao mang ngau nhien truoc!\n");
+            }
+            else {
+                quick_sort(a, 0, n - 1);
+                printf("Mang sau khi sap xep tang dan bang Quick Sort: ");
+                print_array(a, n);
+            }
+            break;
+
+        case 9:
+            printf("Thoat chuong trinh.\n");
+            exit(0);
+
+        default:
+            printf("Lua chon khong hop le!\n");
+            break;
+        }
+    }
+
+    return 0;
 }
