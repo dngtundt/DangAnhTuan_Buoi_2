c7153fd7b4516cb65858a91b47f0d8d519c4124c
dngtundt <dtun11111@gmail.com>
2024-07-17 14:15:48 +0700

    add linear search and interchange sort to Phan 2 BT 1

--- DangAnhTuan_Buoi_2/Phan_2_BT_1.cpp
+++ DangAnhTuan_Buoi_2/Phan_2_BT_1.cpp
@@ -19,3 +19,25 @@ void generate_even_array(int* a, int* b, int n, int* m) {
         }
     }
 }
+// Hàm tìm kiếm tuyến tính
+int linear_search(int* a, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Hàm sắp xếp Interchange Sort
+void interchange_sort(int* a, int n, int ascending) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (ascending ? a[i] > a[j] : a[i] < a[j]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
