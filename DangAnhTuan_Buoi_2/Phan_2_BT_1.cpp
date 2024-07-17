b5f086d4518f89c683c28b1839ddae2b31a5d34e
dngtundt <dtun11111@gmail.com>
2024-07-17 14:06:11 +0700

    first commit added generate even and random array

---
+++ DangAnhTuan_Buoi_2/Phan_2_BT_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Hàm tạo mảng ngẫu nhiên
+void generate_random_array(int* a, int n, int min, int max) {
+    for (int i = 0; i < n; i++) {
+        a[i] = min + rand() % (max - min + 1);
+    }
+}
+
+// Hàm tạo mảng chứa toàn số chẵn
+void generate_even_array(int* a, int* b, int n, int* m) {
+    *m = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
+            b[*m] = a[i];
+            (*m)++;
+        }
+    }
+}
